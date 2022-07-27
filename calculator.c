c4c86cddaec671f2108d062d59b5762ecfcf6ff5
kirtan <kirtanjain0504@gmail.com>
2022-07-27 11:32:54 +0530

    Added Subtraction

--- calculator.c
+++ calculator.c
@@ -4,7 +4,7 @@ int main() {
 
   int op;
   double first, second;
-  printf("Enter your choice 1. Addition ");
+  printf("Enter your choice 1. Addition 2. Subtraction");
   scanf("%d", &op);
   printf("Enter two operands: ");
   scanf("%lf %lf", &first, &second);
@@ -12,6 +12,9 @@ int main() {
   switch (op) {
     case 1:
       printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      break;
+    case 2:
+      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
       break;
     default:
       printf("Error! Option chosen is not correct");
