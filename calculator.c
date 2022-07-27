ae13b50b39136c312cfd3f51e976578a9c117bdf
kirtan <kirtanjain0504@gmail.com>
2022-07-27 10:46:27 +0530

    Calculator with addition

---
+++ calculator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+  int op;
+  double first, second;
+  printf("Enter your choice 1. Addition ");
+  scanf("%d", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
+
+  switch (op) {
+    case 1:
+      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      break;
+    default:
+      printf("Error! Option chosen is not correct");
+  }
+
+  return 0;
+}
