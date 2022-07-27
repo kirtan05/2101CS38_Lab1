344add9565525d501ec7533db5425077f91e9c9c
kirtan <kirtanjain0504@gmail.com>
2022-07-27 11:40:49 +0530

    Added Division

--- calculator.c
+++ calculator.c
@@ -4,7 +4,7 @@ int main() {
 
   int op;
   double first, second;
-  printf("Enter your choice 1. Addition 2. Subtraction 3. Multiplication ");
+  printf("Enter your choice 1. Addition 2. Subtraction 3. Multiplication 4. Divsion  ");
   scanf("%d", &op);
   printf("Enter two operands: ");
   scanf("%lf %lf", &first, &second);
@@ -18,6 +18,9 @@ int main() {
       break;
      case 3:
       printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      break;
+     case 4:
+      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
       break;
     default:
       printf("Error! Option chosen is not correct");
